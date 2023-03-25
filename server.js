17ae431532b7ba009ae5d35635eb70dd50ae8db7
periakteon <masumgokyuz@gmail.com>
2023-03-25 18:02:16 +0300

    edited index.js

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 // .env oluşturmayı unutma (PORT:3000)
 
 const express = require("express");
-const dotenv = require("dotenv").config();
-const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const connectDb = require("./config/dbConnection");
 const PORT = process.env.PORT || 3001;
+const Article = require("./models/articleModel");
 const articleRouter = require("./routes/articles");
 const app = express();
 
-/* 
-EJS modülü template dosyaları görebilmek için varsayılan olarak views klasörünün içerisindeki
-.ejs uzantılı dosyalara bakar.
+dotenv.config();
 
-view engine ile ejs kodlarını HTML'e çeviririz, daha dinamik sayfalar için
-*/
 app.set("view engine", "ejs");
 
-//  veritabanı bağlantısı için asenkron fonksiyon oluşturuyoruz.
-//  önce veritabanı bağlantısının sağlanması için connectDb() fonksiyonunu await ediyoruz
-//  await edilen fonksiyon çözüldüğünde, yani Promise resolve olduğunda port dinlemeyi başlatıyoruz
 const startApp = async () => {
   try {
     await connectDb();
@@ -32,31 +25,13 @@ const startApp = async () => {
 
 startApp();
 
-/*
-
-the difference is express.json() is a body parser for post request except html post form and express.urlencoded({extended: false}) is a body parser for html post form.
-
-express.json() is a built express middleware that convert request body to JSON.
-express.urlencoded() just like express.json() converts request body to JSON, it also carries out some other functionalities like: converting form-data to JSON etc.
-*/
-
 app.use(express.urlencoded({ extended: false }));
 
 // "/" adresine istek geldiğinde views klasörünün içerisindeki "index"i render et
-app.get("/", (req, res) => {
-  // anasayfada çıkacak olan yazıları nesne olarak engine'e göndermek için
-  const articles = [
-    {
-      title: "Test Article",
-      createdAt: new Date(),
-      description: "test description",
-    },
-    {
-      title: "Test Article 2",
-      createdAt: new Date(),
-      description: "test description 2",
-    },
-  ];
+app.get("/", async (req, res) => {
+  //veritabanından tüm yazıları bulup, en yeni olanlar en üstte görünecek şekilde alıyoruz
+  const articles = await Article.find().sort({createdAt: "descending"});
+  
   // anasayfamızı articles view'inin içerisindeki index.ejs olarak belirledik
   // tüm yazıları almak için forEach kullanacağız, bu nedenle yukarıdaki articles dizisini "articles" olarak aktarıyoruz: ejs ile kullanmak için
   res.render("articles/index", { articles: articles });
