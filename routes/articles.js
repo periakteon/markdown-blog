3fcdd1b571298202998003979e97819bad54ad07
periakteon <masumgokyuz@gmail.com>
2023-03-25 13:28:32 +0300

    router.post added for creating new articles

--- routes/articles.js
+++ routes/articles.js
@@ -6,4 +6,7 @@ router.get("/new", (req, res) => {
   res.render("articles/new");
 });
 
+// yeni yazı eklemek için ".../articles" adresine gelen POST isteklerini yönetiyoruz
+router.post("/", (req, res) => {});
+
 module.exports = router;
